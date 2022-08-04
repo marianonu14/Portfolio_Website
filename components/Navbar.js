c833564e63e7171e8fce2460ca9a228f4253068f
Mariano <marianonunezma@gmail.com>
2022-08-04 07:50:30 -0300

    First Commit

---
+++ components/Navbar.js
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import { AiOutlineMenu } from "react-icons/ai";
+
+const Navbar = () => {
+    return ( 
+        <div className="fixed w-full h-20 shadow-xl z-[100]">
+            <div className="flex justify-between items-center w-full h-full px-10 2xl:px-16">
+            <Image 
+            src="/../public/assets/logowebsite.png" 
+            alt="Website Logo"
+            width="80" 
+            height="60"/>
+            <div>
+                <ul className="hidden md:flex">
+                    <Link href="/">
+                        <li className="ml-10 text-md uppercase hover:border-b">Home</li>
+                    </Link>
+                    <Link href="/">
+                        <li className="ml-10 text-md uppercase hover:border-b">About</li>
+                    </Link>
+                    <Link href="/">
+                        <li className="ml-10 text-md uppercase hover:border-b">Skills</li>
+                    </Link>
+                    <Link href="/">
+                        <li className="ml-10 text-md uppercase hover:border-b">Projects</li>
+                    </Link>
+                    <Link href="/">
+                        <li className="ml-10 text-md uppercase hover:border-b">Contact</li>
+                    </Link>
+                </ul>
+                <div className="md:hidden">
+                    <AiOutlineMenu size={25}/>
+                </div>
+            </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Navbar;
