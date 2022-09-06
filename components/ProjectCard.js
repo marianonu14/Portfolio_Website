6ba6aadc58bb7803d920cb14f5252983f7963a20
Mariano <marianonunezma@gmail.com>
2022-09-06 17:31:20 -0300

    Adding new project

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -4,8 +4,8 @@ import Link from "next/link";
 const ProjectCard = (props) => {
     return ( 
         <div className="relative h-auto w-fix flex justify-center items-center group mb-10" >
-            <Image src={props.image} alt="Project_1-Image" height={600} width={400} objectFit='cover' objectPosition='center' className="relative rounded-xl group-hover:opacity-20 opacity-100" />
-            <div className="hidden group-hover:flex flex-col justify-between h-[75%] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <Image src={props.image} alt="Project_1-Image" height={600} width={350} objectFit='cover' objectPosition='center' className="relative rounded-xl group-hover:opacity-20 opacity-100" />
+            <div className="absolute hidden group-hover:flex flex-col justify-between h-[75%] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
                 <h3 className="text-center text-xl sm:text-4xl tracking-wider mb-10 text-grey-400 uppercase">{props.title}</h3>
                 <p className="mb-10 text-center text-lg">{props.info}</p>
                 <Link href={props.link}>
--- components/Projects.js
+++ components/Projects.js
@@ -14,7 +14,7 @@ const Projects = () => {
             <div className="max-w-[1240px] mx-auto flex flex-col justify-start h-full gap-10">
                 <p className="text-2xl font-bold uppercase mt-16">Projects</p>
                 <h2 className="text-blue-600">What I ve Built</h2>
-                <div className="grid xl:grid-cols-3 gap-10">
+                <div className="flex flex-wrap justify-evenly gap-10">
                     <ProjectCard title="Formula 1 App" link={'https://formula1-api.vercel.app/'} image={formula1Img} info={infoFormulaOne}/>
                     <ProjectCard title="Blogger App" link={'https://blogger-app-seven.vercel.app/'} image={bloggerImg} info={infoBlogger}/>
                     <ProjectCard title="E-Guitar" link={'https://e-guitar.vercel.app/'} image={eGuitarImg} info={infoEGuitar}/>
