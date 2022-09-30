5d1c52e720b81a062c390358c8f4ffbbc213fbcd
Mariano <marianonunezma@gmail.com>
2022-09-30 15:55:01 -0300

    Adding hover styles to Project Section Button

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -25,10 +25,10 @@ const ProjectCard = (props) => {
                 <p className="mb-10 text-gray-200 text-xl"><span className="font-bold text-cyan-500">Technologies: </span>{props.tech}</p>
                 <div className="flex flex-col lg:flex-row gap-5 pb-5">
                     <Link href={props.link}>
-                        <a target='_blank' rel="noreferrer" className="flex justify-between items-center px-5 lg:w-1/2 text-center text-base sm:text-xl cursor-pointer bg-blue-600 text-white p-3 rounded-xl"><TbBrowserPlus size={35} />Visit the App</a>
+                        <a target='_blank' rel="noreferrer" className="flex justify-between items-center px-5 lg:w-1/2 text-center text-base sm:text-xl cursor-pointer bg-blue-600 hover:bg-white text-white hover:text-blue-600 duration-700 p-3 rounded-xl"><TbBrowserPlus size={35} />Visit the App</a>
                     </Link>   
                     <Link href={props.repo}>
-                        <a target='_blank' rel="noreferrer" className="flex justify-between items-center px-5 lg:w-1/2 text-center text-base sm:text-xl cursor-pointer bg-gray-600 text-white p-3 rounded-xl"><BsGithub size={35} />Go to the Repository</a>
+                        <a target='_blank' rel="noreferrer" className="flex justify-between items-center px-5 lg:w-1/2 text-center text-base sm:text-xl cursor-pointer bg-gray-600 hover:bg-white text-white hover:text-gray-600 duration-700 p-3 rounded-xl"><BsGithub size={35} />Go to the Repository</a>
                     </Link> 
                 </div>
             </div>
